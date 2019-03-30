b2a2ec70f907c0b58fec915217953ece4a70c5b0
KOBAYASHI Daisuke <daisuke.koba@gmail.com>
2019-03-30 18:30:53 +0900

    [Keymap] Add game layer.

--- keyboards/kbdfans/kbd67/rev1/keymaps/koba/keymap.c
+++ keyboards/kbdfans/kbd67/rev1/keymaps/koba/keymap.c
@@ -17,6 +17,7 @@
 #include "keymap_jp.h"
 
 #define RGB_RMO RGB_RMOD
+#define GA_PSCR G(A(KC_PSCR))
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 /* Keymap (Base Layer) Default Layer
@@ -37,7 +38,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,     KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,    KC_O,    KC_P,    JP_AT,   JP_LBRC, JP_RBRC,       KC_PGUP, \
   KC_ZKHK,       KC_A,   KC_S,   KC_D,   KC_F,   KC_G,   KC_H,   KC_J,   KC_K,    KC_L,    KC_SCLN, JP_COLN,       KC_ENT,        KC_PGDN, \
   KC_LSFT, XXXXXXX, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PSCR, \
-  KC_LCTL, KC_LGUI, KC_LALT,        KC_SPC,         MO(1),          KC_SPC,          KC_RALT, KC_RCTL, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT),
+  KC_LCTL, KC_LGUI, KC_LALT,        KC_SPC,         MO(2),          KC_SPC,          KC_RALT, KC_RCTL, XXXXXXX, KC_LEFT, KC_DOWN, KC_RGHT),
+
+/* Keymap Game Layer */
+[1] = LAYOUT_all(
+  _______, _______,_______,_______,_______,_______,_______,_______,_______, _______, _______, _______, _______, _______, _______, _______, \
+  _______,    _______,_______,_______,_______,_______,_______,_______,_______, _______, _______, _______, _______, _______,       _______, \
+  _______,       _______,_______,_______,_______,_______,_______,_______,_______, _______, _______, _______,       _______,       _______, \
+  _______, _______, _______,_______,_______,_______,_______,_______,_______,_______, _______, _______, _______,          _______, GA_PSCR, \
+  _______, XXXXXXX, _______,        _______,        _______,        _______,         _______, _______, _______, _______, _______, _______),
 
   /* Keymap Fn Layer
    * ,----------------------------------------------------------------.
@@ -49,13 +58,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |----------------------------------------------------------------|
    * |        |BL-|BL+|BL |V- |V+ |  +|  -|End|PDn|  _|      |PUp|    |
    * |----------------------------------------------------------------|
-   * |    |    |    | Muhenkan |   | Henkan |Kana|Menu|  |Hom|PDn|End |
+   * |    |Game|    | Muhenkan |   | Henkan |Kana|Menu|  |Hom|PDn|End |
    * `------------------------------------------------'  `------------'
    */
-[1] = LAYOUT_all(
+[2] = LAYOUT_all(
    RESET,  KC_F1,  KC_F2,  KC_F3,  KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,  KC_F10, KC_F11, KC_F12, XXXXXXX,_______,KC_INS,  \
    _______,   RGB_RMO,RGB_MOD,RGB_TOG,RGB_HUD,RGB_HUI,XXXXXXX,XXXXXXX,KC_PSCR,KC_SLCK,KC_PAUS,XXXXXXX,XXXXXXX,XXXXXXX,     _______, \
    KC_CAPS,     KC_VOLD,KC_VOLU,KC_MUTE,RGB_SAD,RGB_SAI,KC_PAST,KC_PSLS,KC_HOME,KC_PGUP,XXXXXXX,XXXXXXX,      KC_PENT,     _______, \
    _______,_______,BL_DEC, BL_INC, BL_TOGG,RGB_VAD,RGB_VAI,KC_PPLS,KC_PMNS,KC_END, KC_PGDN,JP_UNDS,_______,        KC_PGUP,_______, \
-   _______,_______,_______,        KC_MHEN,        _______,        KC_HENK,        KC_KANA,KC_APP, _______,KC_HOME,KC_PGDN,KC_END),
+   _______,TG(1)  ,_______,        KC_MHEN,        _______,        KC_HENK,        KC_KANA,KC_APP, _______,KC_HOME,KC_PGDN,KC_END),
 };
